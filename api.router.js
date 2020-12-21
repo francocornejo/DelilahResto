b224e4cbbf4a7d182f301dd9c2c2deae31dd8e1e
francocornejo <72417677+francocornejo@users.noreply.github.com>
2020-12-20 22:48:52 -0300

    Update api.router.js

--- api.router.js
+++ api.router.js
@@ -1,13 +1,29 @@
-const productosController = require('../controllers/productos.controller')
-const usuariosController = require('../controllers/usuarios.controller')
-const router = require('express').Router()
-const userValidation = require('../validations/users.validation')
+const router = require('express').Router();
+const pedidosController = require('../controllers/pedidos.controller');
+const productosController = require('../controllers/productos.controller');
+const usuariosController = require('../controllers/usuarios.controller');
+const userValidation = require('../validations/users.validation');
 
-
-
+//usuarios
 router.post('/usuarios', userValidation.isAuthorized, userValidation.isAdmin, usuariosController.createUsuarios)
 router.post('/usuarios/login', usuariosController.authenticate)
+
+//productos
 router.post('/productos',userValidation.isAuthorized, userValidation.isAdmin, productosController.createProductos)
 router.get('/productos',userValidation.isAuthorized, productosController.getProductos)
+router.get('/productos/:id', userValidation.isAuthorized, productosController.getProductoById)
+router.put('/productos/:id', userValidation.isAuthorized, userValidation.isAdmin, productosController.updateProduct)
+router.delete('/productos/:id', userValidation.isAuthorized, userValidation.isAdmin, productosController.deleteProduct)
+
+router.post('/pedidos', userValidation.isAuthorized, pedidosController.createPedidos)
+
+
+/* PUSE ESTO ACA PARA IR REVISANDO UNO POR UNO, PARA VER SI ERA ERROR DE TIPEO
+router.post('/pedidos', userValidation.isAuthorized, pedidosController.createPedidos)
+router.get('/pedidos', userValidation.isAuthorized, userValidation.isAdmin, pedidosController.getPedidos)
+router.get('/pedidos/:id', userValidation.isAuthorized, userValidation.isAdmin, pedidosController.getPedidoById)
+router.put('/pedidos/:id', userValidation.isAuthorized, userValidation.isAdmin, pedidosController.updatePedido)
+router.delete('/pedidos/:id', userValidation.isAuthorized, userValidation.isAdmin, pedidosController.deletePedido)
+*/
 
 module.exports = router
